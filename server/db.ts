8fcc43ec62548e456a2cb3b364d90f6b320eb45b
6maxservices <6maxservices@gmail.com>
2026-05-26 20:28:21 +0300

    fix: replace neon-serverless WebSockets with standard pg TCP socket Pool

--- server/db.ts
+++ server/db.ts
@@ -1,5 +1,6 @@
-import { Pool } from '@neondatabase/serverless';
-import { drizzle } from 'drizzle-orm/neon-serverless';
+import pg from 'pg';
+const { Pool } = pg;
+import { drizzle } from 'drizzle-orm/node-postgres';
 import * as schema from "@shared/schema";
 
 // In standard standalone environments, we allow db to be optional and fall back to MemStorage
